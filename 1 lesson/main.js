96bb1199df7439e0368af66de599db98945baabb
Vadym Tcimbaliyk <vadymonua@gmail.com>
2020-07-15 15:42:19 +0300

    Домашня робота

---
+++ 1 lesson/main.js
@@ -0,0 +1,18 @@
+//1
+let city;
+city = 'Київ';
+city = 'Львів';
+let address = city;
+alert(address);
+//2
+let n = 2;
+let result = Math.pow(n, 3);
+console.log(result);
+
+//3 
+let quantityPensil = 4;
+let quantityPen = 6;
+let pensil = 3.5;
+let pen = 5.25;
+let dye = (pensil * quantityPensil) + (pen * quantityPen);
+document.write(dye);
